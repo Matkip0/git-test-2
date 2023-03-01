6ef87693153ca280786ef9f5fe1ba647f7afc61c
Matkip0 <biegel987@gmail.com>
2023-03-01 10:47:30 +0100

    Update Program.cs

--- lommeregner 2/Program.cs
+++ lommeregner 2/Program.cs
@@ -1,20 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("");
-int Nummer1 = 0;
-int Nummer2 = 0;
+double Nummer1 = 0;
+double Nummer2 = 0;
+
 
 
 Console.WriteLine("Indtast det første tal");
-Nummer1 = Convert.ToInt32(Console.ReadLine());
+Nummer1 = Convert.ToDouble(Console.ReadLine());
 
 Console.WriteLine("Indtast det andet tal");
-Nummer2 = Convert.ToInt32(Console.ReadLine());
+Nummer2 = Convert.ToDouble(Console.ReadLine());
 
 Console.WriteLine($"tallene giver {Nummer1 + Nummer2}");
 
 Console.WriteLine("Vælg hvad du vil have numrene til at gøre ved brug af");
 Console.WriteLine("mulighederne er +, -, *, /,");
 string svar = Console.ReadLine();
+
 
 if (svar == ("+"))
 {
@@ -32,3 +34,6 @@ if (svar == ("/"))
 {
     Console.WriteLine($"tallene giver {Nummer1 / Nummer2}");
 }
+
+
+
